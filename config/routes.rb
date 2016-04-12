c8b200d22e5220c5821b317cc9e9550364950a03
Arnold Redoblado <VOODOO15@VOODOO15-MBP.local>
2016-04-11 23:33:06 -0400

    Updated actions in Product controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  resources :products
+  # get 'products/index'
+  #
+  # get 'products/show'
+  #
+  # get 'products/new'
+  #
+  # get 'products/edit'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
